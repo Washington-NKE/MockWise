e81d2eb0c806670554c5fe215a81d28e0f339d9a
Washington <washiem2022@gmail.com>
2025-03-31 00:41:47 +0300

    setup app, file and folder structure, routing and auth pages

---
+++ app/api/vapi/generate/route.tsx
@@ -0,0 +1,13 @@
+`Prepare questions for a job interview.
+        The job role is ${role}.
+        The job experience level is ${level}.
+        The tech stack used in the job is: ${techstack}.
+        The focus between behavioural and technical questions should lean towards: ${type}.
+        The amount of questions required is: ${amount}.
+        Please return only the questions, without any additional text.
+        The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3
+    `;
